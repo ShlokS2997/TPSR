54eb6cfa7285e3c928fef9b6345da6c64dbb6a3d
Shlok Singh <134242409+ShlokS2997@users.noreply.github.com>
2024-09-25 19:02:33 -0400

    Update sklearn_wrapper.py

--- symbolicregression/model/sklearn_wrapper.py
+++ symbolicregression/model/sklearn_wrapper.py
@@ -1,7 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-
 import math, time, copy
 import numpy as np
 import torch
@@ -9,9 +5,9 @@ from collections import defaultdict
 from symbolicregression.metrics import compute_metrics
 from sklearn.base import BaseEstimator
 import symbolicregression.model.utils_wrapper as utils_wrapper
-import traceback
 from sklearn import feature_selection
 from sklearn.model_selection import KFold
+import traceback
 
 def corr(X, y, epsilon=1e-10):
     """
@@ -23,16 +19,16 @@ def corr(X, y, epsilon=1e-10):
     return corr
 
 def get_top_k_features(X, y, k=10):
-    if y.ndim==2:
-        y=y[:,0]
-    if X.shape[1]<=k:
+    if y.ndim == 2:
+        y = y[:, 0]
+    if X.shape[1] <= k:
         return [i for i in range(X.shape[1])]
     else:
         kbest = feature_selection.SelectKBest(feature_selection.r_regression, k=k)
         kbest.fit(X, y)
         scores = kbest.scores_
         top_features = np.argsort(-np.abs(scores))
-        print("keeping only the top-{} features. Order was {}".format(k, top_features))
+        print("Keeping only the top-{} features. Order was {}".format(k, top_features))
         return list(top_features[:k])
 
 def exchange_node_values(tree, dico):
@@ -44,39 +40,34 @@ def exchange_node_values(tree, dico):
 class SymbolicTransformerRegressor(BaseEstimator):
 
     def __init__(self,
-                model=None,
-                max_input_points=10000,
-                max_number_bags=-1,
-                stop_refinement_after=1,
-                n_trees_to_refine=1,
-                rescale=True
-                ):
-
+                 model=None,
+                 max_input_points=10000,
+                 max_number_bags=-1,
+                 stop_refinement_after=1,
+                 n_trees_to_refine=1,
+                 rescale=True):
+        super().__init__()  # Ensure BaseEstimator initialization
+        self.model = model
         self.max_input_points = max_input_points
         self.max_number_bags = max_number_bags
-        self.model = model
         self.stop_refinement_after = stop_refinement_after
         self.n_trees_to_refine = n_trees_to_refine
         self.rescale = rescale
 
     def set_args(self, args={}):
         for arg, val in args.items():
-            assert hasattr(self, arg), "{} arg does not exist".format(arg)
-            setattr(self, arg, val)
+            if hasattr(self, arg):
+                setattr(self, arg, val)
+            else:
+                raise ValueError(f"{arg} arg does not exist")
             
-    def fit(
-        self,
-        X,
-        Y,
-        verbose=False,
-        n_splits=5  # Number of cross-validation folds
-):
+    def fit(self, X, Y, verbose=False, n_splits=5):  # Number of cross-validation folds
         self.start_fit = time.time()
 
         if not isinstance(X, list):
             X = [X]
             Y = [Y]
-            n_datasets = len(X)
+        n_datasets = len(X)
 
         self.top_k_features = [None for _ in range(n_datasets)]
         for i in range(n_datasets):
@@ -111,7 +102,8 @@ class SymbolicTransformerRegressor(BaseEstimator):
 
                 # Run model
                 outputs = self.model(inputs)
-                if verbose: print("Finished forward in {} secs".format(time.time() - forward_time))
+                if verbose: 
+                    print("Finished forward in {} secs".format(time.time() - forward_time))
 
                 # Process candidates
                 refined_candidates = self.refine(X_train, y_train, outputs, verbose=verbose)
@@ -124,12 +116,12 @@ class SymbolicTransformerRegressor(BaseEstimator):
             if candidates:
                 self.tree[input_id] = self.order_candidates(X[input_id], Y[input_id], candidates, metric="_mse", verbose=verbose)
 
-
+        return self  # Ensuring the estimator returns self for scikit-learn compatibility
 
     @torch.no_grad()
     def evaluate_tree(self, tree, X, y, metric):
         numexpr_fn = self.model.env.simplifier.tree_to_numexpr_fn(tree)
-        y_tilde = numexpr_fn(X)[:,0]
+        y_tilde = numexpr_fn(X)[:, 0]
         metrics = compute_metrics({"true": [y], "predicted": [y_tilde], "predicted_tree": [tree]}, metrics=metric)
         return metrics[metric][0]
 
@@ -139,39 +131,40 @@ class SymbolicTransformerRegressor(BaseEstimator):
             if metric not in candidate:
                 score = self.evaluate_tree(candidate["predicted_tree"], X, y, metric)
                 if math.isnan(score): 
-                    score = np.infty if metric.startswith("_") else -np.infty
+                    score = np.inf if metric.startswith("_") else -np.inf
             else:
                 score = candidates[metric]
             scores.append(score)
         ordered_idx = np.argsort(scores)  
-        if not metric.startswith("_"): ordered_idx=list(reversed(ordered_idx))
+        if not metric.startswith("_"): 
+            ordered_idx = list(reversed(ordered_idx))
         candidates = [candidates[i] for i in ordered_idx]
         return candidates
 
     def refine(self, X, y, candidates, verbose):
         refined_candidates = []
-        
-        ## For skeleton model
+
         for i, candidate in enumerate(candidates):
-            candidate_skeleton, candidate_constants =  self.model.env.generator.function_to_skeleton(candidate, constants_with_idx=True)
+            candidate_skeleton, candidate_constants = self.model.env.generator.function_to_skeleton(candidate, constants_with_idx=True)
             if "CONSTANT" in candidate_constants:
                 candidates[i] = self.model.env.wrap_equation_floats(candidate_skeleton, np.random.randn(len(candidate_constants)))
 
         candidates = [{"refinement_type": "NoRef", "predicted_tree": candidate, "time": time.time()-self.start_fit} for candidate in candidates]
         candidates = self.order_candidates(X, y, candidates, metric="_mse", verbose=verbose)
 
-        ## REMOVE SKELETON DUPLICATAS
+        # Remove skeleton duplicates
         skeleton_candidates, candidates_to_remove = {}, []
         for i, candidate in enumerate(candidates):
             skeleton_candidate, _ = self.model.env.generator.function_to_skeleton(candidate["predicted_tree"], constants_with_idx=False)
             if skeleton_candidate.infix() in skeleton_candidates:
                 candidates_to_remove.append(i)
             else:
-                skeleton_candidates[skeleton_candidate.infix()]=1
-        if verbose: print("Removed {}/{} skeleton duplicata".format(len(candidates_to_remove), len(candidates)))
+                skeleton_candidates[skeleton_candidate.infix()] = 1
+        if verbose: 
+            print("Removed {}/{} skeleton duplicates".format(len(candidates_to_remove), len(candidates)))
 
         candidates = [candidates[i] for i in range(len(candidates)) if i not in candidates_to_remove]
-        if self.n_trees_to_refine>0:
+        if self.n_trees_to_refine > 0:
             candidates_to_refine = candidates[:self.n_trees_to_refine]
         else:
             candidates_to_refine = copy.deepcopy(candidates)
@@ -181,12 +174,12 @@ class SymbolicTransformerRegressor(BaseEstimator):
             candidate_skeleton, candidate_constants = self.model.env.generator.function_to_skeleton(candidate["predicted_tree"], constants_with_idx=True)
             try:
                 refined_candidate = refinement_strategy.go(env=self.model.env, 
-                                                        tree=candidate_skeleton, 
-                                                        coeffs0=candidate_constants,
-                                                        X=X,
-                                                        y=y,
-                                                        downsample=1024,
-                                                        stop_after=self.stop_refinement_after)
+                                                           tree=candidate_skeleton, 
+                                                           coeffs0=candidate_constants,
+                                                           X=X,
+                                                           y=y,
+                                                           downsample=1024,
+                                                           stop_after=self.stop_refinement_after)
             except Exception as e:
                 if verbose: 
                     print(e)
@@ -202,7 +195,7 @@ class SymbolicTransformerRegressor(BaseEstimator):
 
         for candidate in candidates:
             if "time" not in candidate:
-                candidate["time"]=time.time()-self.start_fit
+                candidate["time"] = time.time() - self.start_fit
         return candidates
 
     def __str__(self):
@@ -212,74 +205,21 @@ class SymbolicTransformerRegressor(BaseEstimator):
                     print(gen)
         return "Transformer"
 
-    def retrieve_refinements_types(self):
-        # return ["BFGS", "NoRef"]
-        return ["BFGS"]
-
     def exchange_tree_features(self):
         top_k_features = self.top_k_features
         for dataset_id, candidates in self.tree.items():
             exchanges = {}
             for i, feature in enumerate(top_k_features[dataset_id]):
-                exchanges["x_{}".format(i)]="x_{}".format(feature)
+                exchanges["x_{}".format(i)] = "x_{}".format(feature)
             for candidate in candidates:
                 candidate["relabed_predicted_tree"] = exchange_node_values(candidate["predicted_tree"], exchanges)
 
     def retrieve_tree(self, refinement_type=None, dataset_idx=0, all_trees=False, with_infos=False):
         self.exchange_tree_features()
-        if dataset_idx == -1: idxs = [_ for _ in range(len(self.tree))] 
-        else: idxs = [dataset_idx]
+        if dataset_idx == -1: 
+            idxs = [_ for _ in range(len(self.tree))] 
+        else: 
+            idxs = [dataset_idx]
         best_trees = []
         for idx in idxs:
-            best_tree = copy.deepcopy(self.tree[idx])
-            if best_tree and refinement_type is not None:
-                best_tree = list(filter(lambda gen: gen["refinement_type"]==refinement_type, best_tree))
-            if not best_tree:
-                if with_infos:
-                    best_trees.append({"predicted_tree": None, "refinement_type": None, "time": None})
-                else:
-                    best_trees.append(None)
-            else:
-                if with_infos:
-                    if all_trees:
-                        best_trees.append(best_tree)
-                    else:
-                        best_trees.append(best_tree[0])
-                else:
-                    if all_trees:
-                        best_trees.append([best_tree[i]["predicted_tree"] for i in range(len(best_tree))])
-                    else:
-                        best_trees.append(best_tree[0]["predicted_tree"])
-        if dataset_idx != -1: 
-            return best_trees[0]
-        else: return best_trees
-
-
-    def predict(self, X, refinement_type=None, tree_idx=0, batch=False):        
-
-        if not isinstance(X, list):
-            X = [X]
-        for i in range(len(X)):
-            X[i]=X[i][:,self.top_k_features[i]]
-
-        res = []
-        if batch:
-            tree = self.retrieve_tree(refinement_type=refinement_type, dataset_idx = -1)
-            for tree_idx in range(len(tree)):
-                X_idx = X[tree_idx]
-                if tree[tree_idx] is None: 
-                    res.append(None)
-                else:   
-                    numexpr_fn = self.model.env.simplifier.tree_to_numexpr_fn(tree[tree_idx])
-                    y = numexpr_fn(X_idx)[:,0]
-                    res.append(y)
-            return res
-        else:
-            X_idx = X[tree_idx]
-            tree = self.retrieve_tree(refinement_type=refinement_type, dataset_idx = tree_idx)
-            if tree is not None:
-                numexpr_fn = self.model.env.simplifier.tree_to_numexpr_fn(tree)
-                y = numexpr_fn(X_idx)[:,0]
-                return y
-            else:
-                return None
+            best_tree = copy.deepcopy(self.tree
